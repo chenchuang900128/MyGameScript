79f07d27f6d2dd2cb1350f9a5d1ff5ae70fa1bc7
chenchuang28 <874938129@qq.com>
2018-10-18 22:09:43 +0800

    游戏SDK
    游戏SDK

--- MyGameScript/ViewController.m
+++ MyGameScript/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <GameScript/MBSLoginAlert.h>
+#import <GameScript/MBSSuspendControl.h>
+
+#define kScreenWidth [[UIScreen mainScreen] bounds].size.width
+#define kScreenHeight [[UIScreen mainScreen] bounds].size.height
 
 @interface ViewController ()
+
+@property(nonatomic,strong)MBSLoginAlert *regAlert;
+@property(nonatomic,strong)MBSLoginAlert *logAlert;
 
 @end
 
@@ -16,9 +24,130 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setFrame:CGRectMake(20, 200, kScreenWidth - 40, 40)];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [btn setTitle:@"开始游戏" forState:UIControlStateNormal];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)btnClick:(UIButton *)sender{
+    
+    
+    sender.hidden = YES;
+#if 1
+    // 注册弹框
+    self.regAlert = [[MBSLoginAlert alloc] initWithRegisterTitle:@"注册账号" loginBlock:^(id object) {
+        
+        // 跳老用户登录
+        [self.regAlert hide];
+        // 登录弹框
+        self.logAlert = [[MBSLoginAlert alloc] initWithTitle:@"用户登录" loginBlock:^(id object) {
+            
+            [self.logAlert hide];
+            // 显示悬浮窗
+            MBSSuspendControl *tmpControl = [[MBSSuspendControl alloc] initWithClickBlock:^(NSUInteger index) {
+                
+                NSLog(@"点击第几个%lu",index);
+                switch (index) {
+                    case 0:
+                    {
+                        
+                    }
+                        break;
+                    case 1:
+                    {
+                        
+                    }
+                        break;
+                    case 2:
+                    {
+                        
+                    }
+                        break;
+                    case 3:
+                    {
+                        
+                    }
+                        break;
+                    case 4:
+                    {
+                        
+                    }
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }];
+            [tmpControl show];
+            
+        } andRegisterBlock:^(id object) {
+            
+            [self.logAlert hide];
+            [self.regAlert show];
+        }];
+        [self.logAlert show];
+        
+        
+    } andRegisterBlock:^(id object) {
+        
+        /*
+         请求交易成功
+         */
+        
+        [self.regAlert hide];
+        
+        
+        // 显示悬浮窗
+        MBSSuspendControl *tmpControl = [[MBSSuspendControl alloc] initWithClickBlock:^(NSUInteger index) {
+            
+            NSLog(@"点击第几个%lu",index);
+
+            switch (index) {
+                case 0:
+                    {
+                        
+                    }
+                    break;
+                case 1:
+                {
+                    
+                }
+                    break;
+                case 2:
+                {
+                    
+                }
+                    break;
+                case 3:
+                {
+                    
+                }
+                    break;
+                case 4:
+                {
+                    
+                }
+                    break;
+                    
+                default:
+                    break;
+            }
+        }];
+        [tmpControl show];
+        
+    }];
+    
+    [self.regAlert show];
+    
+#endif
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
