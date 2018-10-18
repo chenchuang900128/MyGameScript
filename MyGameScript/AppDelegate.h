418343af9c64b68ffde3c8a0ccf223e530f59a84
chenchuang28 <874938129@qq.com>
2018-10-18 21:35:40 +0800

    Initial Commit

---
+++ MyGameScript/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGameScript
+//
+//  Created by 陈创 on 2018/10/18.
+//  Copyright © 2018年 陈创. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
